9ec794d291e0046a5793b33f51479ab2325edff5
Sarisha.tk <sarishakrishna58@gmail.com>
2025-07-26 08:24:41 +0530

    error resolved

--- reverse.py
+++ reverse.py
@@ -6,4 +6,12 @@
 
 # 2. To check the number is prime or not
 
-num 
+num = int(input("Enter a number : "))
+if num <= 0:
+    print("Number is not a prime number")
+for i in range(2,int(num**0.5),+1):
+    if i % 2 ==0:
+        print("number is not a prime number")
+        break
+else:
+    print("it is a prime number")
