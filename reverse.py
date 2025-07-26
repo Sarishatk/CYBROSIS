91295e5558564738a47824d83c7c6bc5c300a40d
Sarisha.tk <sarishakrishna58@gmail.com>
2025-07-26 08:12:58 +0530

    prime or not

--- reverse.py
+++ reverse.py
@@ -1,3 +1,9 @@
-company = "cybrosys"
-reverse ="". join(reversed(company))
-print(reverse)
+
+# 1.  reverse of a string
+# company = "cybrosys"
+# reverse ="". join(reversed(company))
+# print(reverse)
+
+# 2. To check the number is prime or not
+
+num 
